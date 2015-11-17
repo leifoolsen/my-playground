fb88a140a6a5a66ed591273e5cc5cce14c29d80d
Leif Olsen <leif.o.olsen@gmail.com>
2015-11-17 19:32:36 +0100

    Added hello-react

--- hello-react/app/main.jsx
+++ hello-react/app/main.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+/*
 class HelloWorld extends Component {
   render() {
-    return <div>Hello  {this.props.name}</div>;
+    return <div>Hello {this.props.name} (class)</div>;
   }
 }
-
 ReactDOM.render(<HelloWorld  name="LOL" />, document.querySelector('#react-mount'));
+*/
+
+// Staeless function using ES6 arrow syntax
+// See: https://facebook.github.io/react/docs/reusable-components.html
+var HelloFatPointer = (props) => <div>Hello {props.name} (Staeless function using ES6 arrow syntax)</div>;
+
+ReactDOM.render(<HelloFatPointer  name="LOL" />, document.querySelector('#react-mount'));
