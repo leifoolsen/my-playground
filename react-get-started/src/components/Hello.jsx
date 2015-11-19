ba57328fe2e2557ae0be3d177b7112f6d6b0fdb4
m95265 <LeifOddvar.Olsen@skatteetaten.no>
2015-11-19 12:07:59 +0100

    Updated react-get-started

---
+++ react-get-started/src/components/Hello.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
+
+export default class Hello extends Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
