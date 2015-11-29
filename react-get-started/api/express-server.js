b695b0b259f6187fa66f3b9d8d8e7d8a33186e45
Leif Olsen <leif.o.olsen@gmail.com>
2015-11-29 22:57:39 +0100

    Completed React Tutorial: https://facebook.github.io/react/docs/tutorial.html. Reafctored code to ES6.

--- react-get-started/api/express-server.js
+++ react-get-started/api/express-server.js
@@ -14,10 +14,11 @@ const port = 8081;
 // Use a copy of the original file
 const dataPath = path.resolve(__dirname, 'data');
 const commentsFile = path.resolve(dataPath, 'comments.json.tmp'); // '.tmp' is git-ignored
+fs.writeFileSync(commentsFile, fs.readFileSync(path.resolve(dataPath, 'comments.json')));
 
 
 // Folder to to serve public files
-// app.use('/', express.static(path.resolve(__dirname, 'public')));
+//app.use('/', express.static(path.resolve(__dirname, 'public')));
 
 
 // node.js middleware for handling JSON, Raw, Text and URL encoded form data
@@ -25,8 +26,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Run the server
-const server = app.listen(port, 'localhost', () => {
-  fs.writeFileSync(commentsFile, fs.readFileSync(path.resolve(dataPath, 'comments.json')));
+app.set('port', port);
+
+const server = app.listen(app.get('port'), 'localhost', () => {
   console.log(`Server running @ http://${server.address().address}:${server.address().port}. Comments: ${commentsFile}`);
 });
 
@@ -39,7 +41,8 @@ app.get('/yo', (req, res) => {
 app.get('/api/comments.json', (req, res) => {
   fs.readFile(commentsFile, (err, data) => {
     if(err) {
-      res.status(404).send('404: "comments.json" not found!');
+      console.error(err);
+      res.status(500).send(JSON.stringify(err));
     }
     else {
       res.setHeader('Content-Type', 'application/json');
@@ -49,13 +52,27 @@ app.get('/api/comments.json', (req, res) => {
 });
 
 app.post('/api/comments.json', (req, res) => {
+
+  // Maybe rewite this messy code
   fs.readFile(commentsFile, (err, data) => {
-    const comments = JSON.parse(data);
-    comments.push(req.body);
-    fs.writeFile(commentsFile, JSON.stringify(comments, null, 4), function(err) {
-      res.setHeader('Content-Type', 'application/json');
-      res.setHeader('Cache-Control', 'no-cache');
-      res.send(JSON.stringify(req.body));
-    });
+    if(err) {
+      console.error(err);
+      res.status(500).send(JSON.stringify(err));
+    }
+    else {
+      const comments = JSON.parse(data);
+      comments.push(req.body);
+      fs.writeFile(commentsFile, JSON.stringify(comments, null, 4), (err) => {
+        if(err) {
+          console.error(err);
+          res.status(500).send(JSON.stringify(err));
+        }
+        else {
+          res.setHeader('Content-Type', 'application/json');
+          res.setHeader('Cache-Control', 'no-cache');
+          res.send(JSON.stringify(req.body));
+        }
+      });
+    }
   });
 });
