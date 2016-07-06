3b90458fc161ee32b9ba884be45c3404dff76f33
Leif Oddvar Olsen <LeifOddvar.Olsen@skatteetaten.no>
2016-07-06 14:51:59 +0200

    destructuring kata

--- es2015-katas/test/sample/destructuring.spec.js
+++ es2015-katas/test/sample/destructuring.spec.js
@@ -22,4 +22,38 @@ describe('destructuring', () => {
     expect(b).to.equal(3);
   });
 
+  it('should have an undefined property', () => {
+    const foo = { bar: 'pony' }
+    const {baz} = foo
+    expect(baz).to.be.undefined;
+  });
+
+  it('should access a nested property', () => {
+    let foo = { bar: { deep: 'pony', dangerouslySetInnerHTML: 'lol' } }
+    let {bar: { deep, dangerouslySetInnerHTML: sure }} = foo
+    expect(deep).to.equal('pony');
+    expect(sure).to.equal('lol');
+  });
+
+  it('should throw error if deep nested property does not exist', () => {
+    expect( () => {
+      let {foo:{bar}} = {baz: 'ouch'};
+    }).to.throw('Cannot read');
+  });
+
+  it('should swap properties', () => {
+    let left = 10
+    let right = 20
+    if (right > left) {
+      [left, right] = [right, left]
+    }
+    expect(left).to.equal(20);
+    expect(right).to.equal(10);
+  });
+
+  it('should skip elements', () => {
+    var [,,a,b] = [1,2,3,4,5]
+    expect(a).to.equal(3);
+    expect(b).to.equal(4);
+  });
 });
