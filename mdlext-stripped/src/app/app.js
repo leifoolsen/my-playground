dd419e4a07a2c483b8a2cdb03ec6bfa5c5c92718
Leif Olsen <leif.o.olsen@gmail.com>
2017-01-18 00:40:22 +0100

    Deleted mdlext-stripped project

--- mdlext-stripped/src/app/app.js
+++
@@ -1,5 +0,0 @@
-const run = () => {
-  // eslint-disable-next-line no-console
-  console.log('Application started!');
-};
-export default run;
--- mdlext-stripped/src/vendor.js
+++
@@ -1,6 +0,0 @@
-// List your vendors
-
-import 'material-design-lite/material';
-import 'mdl-ext';
-
-// +++ other 3'rd party
